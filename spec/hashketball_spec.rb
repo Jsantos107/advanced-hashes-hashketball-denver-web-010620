1866d48cbf478d776782a5c300cfab9b844ec617
Jorge Santos <neneluis55@gmail.com>
2019-12-31 05:57:20 +0000

    Done.

--- spec/hashketball_spec.rb
+++ spec/hashketball_spec.rb
@@ -26,7 +26,7 @@ describe 'hashketball' do
   describe '#num_points_scored' do
 
     it 'knows the number of points scored by each player' do
-      expect(num_points_scored("Jeff Adrien")).to eq(10)
+         expect(num_points_scored("Jeff Adrien")).to eq(10)
       expect(num_points_scored("Bismack Biyombo")).to eq(12)
       expect(num_points_scored("DeSagna Diop")).to eq(24)
       expect(num_points_scored("Ben Gordon")).to eq(33)
@@ -35,24 +35,26 @@ describe 'hashketball' do
       expect(num_points_scored("Reggie Evans")).to eq(12)
       expect(num_points_scored("Brook Lopez")).to eq(17)
       expect(num_points_scored("Mason Plumlee")).to eq(26)
-      expect(num_points_scored("Jason Terry")).to eq(19)
-    end
+      expect(num_points_scored("Jason Terry")).to eq(19) 
+   
 
-  end
+    end
+end
 
   describe '#shoe_size' do
 
     it 'knows the shoe size of each player' do
+      expect(shoe_size("Alan Anderson")).to eq(16)
+      expect(shoe_size("Reggie Evans")).to eq(14)
+      expect(shoe_size("Brook Lopez")).to eq(17)
+      expect(shoe_size("Mason Plumlee")).to eq(19)
+      expect(shoe_size("Jason Terry")).to eq(15) 
       expect(shoe_size("Jeff Adrien")).to eq(18)
       expect(shoe_size("Bismack Biyombo")).to eq(16)
       expect(shoe_size("DeSagna Diop")).to eq(14)
       expect(shoe_size("Ben Gordon")).to eq(15)
       expect(shoe_size("Kemba Walker")).to eq(15)
-      expect(shoe_size("Alan Anderson")).to eq(16)
-      expect(shoe_size("Reggie Evans")).to eq(14)
-      expect(shoe_size("Brook Lopez")).to eq(17)
-      expect(shoe_size("Mason Plumlee")).to eq(19)
-      expect(shoe_size("Jason Terry")).to eq(15)
+     
     end
 
   end
@@ -90,7 +92,6 @@ describe 'hashketball' do
   end
 
   describe '#player_stats' do
-
     let(:jeff_stats) {
       {
         :number => 4,
@@ -220,8 +221,7 @@ describe 'hashketball' do
         :slam_dunks => 1
       }
     }
-
-    it 'returns all stats for a given player' do
+  it 'returns all stats for a given player' do
       expect(player_stats("Jeff Adrien")).to eq(jeff_stats)
       expect(player_stats("Bismack Biyombo")).to eq(bismak_stats)
       expect(player_stats("DeSagna Diop")).to eq(desagna_stats)
@@ -232,9 +232,9 @@ describe 'hashketball' do
       expect(player_stats("Brook Lopez")).to eq(brook_stats)
       expect(player_stats("Mason Plumlee")).to eq(mason_stats)
       expect(player_stats("Jason Terry")).to eq(jason_stats)
-    end
-
-  end
+    end 
+end
+  
 
   describe '#big_shoe_rebounds' do
 
